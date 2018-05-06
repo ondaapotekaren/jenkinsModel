fad1e4368cb44d6f03383bcd242af5714237668d
ondaapotekaren <viktor_berglund@hotmail.com>
2018-05-06 12:34:13 +0200

    add break statement

--- model2.py
+++ model2.py
@@ -1,4 +1,5 @@
 import numpy
+import sys
 
 #curList = [0] * executors
 
@@ -6,81 +7,92 @@ import numpy
 
 #queue = [1,3,4,5,6,5,9,4,5,2,3]
 
-s =  (numpy.random.pareto(2,100) + 1)
+nmbrOfTests = int(sys.argv[1])
 
-queue = []
+makespan = 0
 
-for i in s:
-	queue.append(int(i*100))
+for x in range(nmbrOfTests):
 
-print("The queue: " + str(queue)+'\n') 
-print("max entry in queue: " + str(max(queue))+'\n')
+	s =  (numpy.random.pareto(2,100) + 1)
 
-nodes = 2
-nodeList = []
-executors = 10
+	queue = []
 
+	for i in s:
+		queue.append(int(i*100))
 
-# (currently running,accumulated time)
+	#print("The queue: " + str(queue)+'\n') 
+	#print("max entry in queue: " + str(max(queue))+'\n')
 
-for i in range(nodes):
-	nodeList.append(([0]*executors,[0]*executors))
-
-while len(queue) > 0:
+	nodes = 2
+	nodeList = []
+	executors = 10
 
 
-	freeSlots = False
-	for i in range(len(nodeList)):
-		for ii in range(len(nodeList[i][0])):
-			if nodeList[i][0][ii] == 0:
-				freeSlots = True
+	# (currently running,accumulated time)
 
-	if(freeSlots):
+	for i in range(nodes):
+		nodeList.append(([0]*executors,[0]*executors))
 
-		# add from queue	
+	while len(queue) > 0:
 
-		curNode = 0
-		highestScore = 0
-			
-		# load balancer
-		# find node
+
+		freeSlots = False
 		for i in range(len(nodeList)):
-			# calc score
-			busy = 0
 			for ii in range(len(nodeList[i][0])):
-				if nodeList[i][0][ii] != 0:
-					busy = busy + 1
-			score = float(len(nodeList[i][0]))/(float(busy)+0.5) 
-			if  score > highestScore:
-				highestScore = score
-				curNode = i
-
-		# add job from queue to a node,first available executor
-
-		temp = int(queue.pop(0))
-		for i in range(len(nodeList[curNode][0])):
-			if nodeList[curNode][0][i] == 0:
-				nodeList[curNode][0][i] = temp
-				nodeList[curNode][1][i] += temp
+				if nodeList[i][0][ii] == 0:
+					freeSlots = True
+					break
+			if freeSlots:
 				break
 
-		
-	else:
+		if freeSlots:
 
-	# if there are no free slots 
-	# subtract one time unit from all executors
+			# add from queue	
 
-		for i in range(len(nodeList)):
-			for ii in range(len(nodeList[i][0])):
-					nodeList[i][0][ii] -= 1
+			curNode = 0
+			highestScore = 0
+				
+			# load balancer
+			# find node
+			for i in range(len(nodeList)):
+				# calc score
+				busy = 0
+				for ii in range(len(nodeList[i][0])):
+					if nodeList[i][0][ii] != 0:
+						busy = busy + 1
+				score = float(len(nodeList[i][0]))/(float(busy)+0.5) 
+				if  score > highestScore:
+					highestScore = score
+					curNode = i
+
+			# add job from queue to a node,first available executor
+
+			temp = int(queue.pop(0))
+			for i in range(len(nodeList[curNode][0])):
+				if nodeList[curNode][0][i] == 0:
+					nodeList[curNode][0][i] = temp
+					nodeList[curNode][1][i] += temp
+					break
+
+			
+		else:
+
+		# if there are no free slots 
+		# subtract one time unit from all executors
+
+			for i in range(len(nodeList)):
+				for ii in range(len(nodeList[i][0])):
+						nodeList[i][0][ii] -= 1
 
 
-print(nodeList)
+	#print(nodeList)
 
-curMax = 0
-for i in nodeList:
-	x = max(i[1])
-	if x > curMax:
-		curMax = x
+	curMax = 0
+	for i in nodeList:
+		x = max(i[1])
+		if x > curMax:
+			curMax = x
 
-print("Makespan: "+str(curMax))
+	makespan += curMax
+
+print("Makespan: "+str(float(makespan)/nmbrOfTests))
